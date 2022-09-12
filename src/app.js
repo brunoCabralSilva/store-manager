41e756246918795171ca7de58b617f88307853c2
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-09-12 15:10:32 -0300

    Erro ao executar o nodemon

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const products = require('./controllers/products');
 
 const app = express();
+
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
 
+app.use('/products', products);
 // não remova essa exportação: é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo server.js para executar sua aplicação 

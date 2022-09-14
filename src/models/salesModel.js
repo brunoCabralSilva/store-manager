bc018ac205058e3eb6555bd168162e2a566dcde8
brunoCabralSilva <bruno.cabral.silva2018@gmail.com>
2022-09-14 18:31:27 -0300

    Requisito 8

--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -30,8 +30,9 @@ const allSalesModel = async () => {
         s.date,
         sp.product_id AS productId,
         sp.quantity
-        FROM sales AS s
-        INNER JOIN sales_products AS sp
+        FROM StoreManager.sales AS s
+        INNER JOIN StoreManager.sales_products AS sp
+        ON s.id = sp.sale_id
         ORDER BY id, product_id`,
     );
   return results;
@@ -45,6 +46,7 @@ const oneSaleModel = async (id) => {
       sp.quantity
       FROM StoreManager.sales AS s
       INNER JOIN sales_products AS sp
+      ON s.id = sp.sale_id
       WHERE s.id = ?
       ORDER BY id, product_id`, [id],
   );
